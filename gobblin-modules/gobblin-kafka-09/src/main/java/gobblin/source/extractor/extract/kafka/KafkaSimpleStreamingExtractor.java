df4d762619d430d9e37f3c7b2c486d6e81a7af8a
Shrikanth Shankar <shshankar@linkedin.com>
2017-01-08 13:22:03 -0800

    first cut at kafka streaming. also added emacs temp files to .gitignore

---
+++ gobblin-modules/gobblin-kafka-09/src/main/java/gobblin/source/extractor/extract/kafka/KafkaSimpleStreamingExtractor.java
@@ -0,0 +1,212 @@
+/*
+ * Copyright (C) 2014-2016 NerdWallet All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package gobblin.source.extractor.extract.kafka;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.hadoop.util.hash.Hash;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+
+import com.google.common.base.Preconditions;
+import com.google.gson.JsonElement;
+
+import kafka.Kafka;
+import kafka.common.TopicAndPartition;
+
+import gobblin.configuration.ConfigurationKeys;
+import gobblin.configuration.WorkUnitState;
+import gobblin.configuration.State;
+import gobblin.metrics.kafka.KafkaSchemaRegistry;
+import gobblin.metrics.kafka.SchemaRegistryException;
+import gobblin.source.extractor.DataRecordException;
+import gobblin.source.extractor.Extractor;
+import gobblin.source.extractor.Watermark;
+import gobblin.source.extractor.extract.EventBasedExtractor;
+import gobblin.source.extractor.StreamingExtractor;
+import gobblin.source.extractor.RecordEnvelope;
+import gobblin.source.extractor.CheckpointableWatermark;
+import gobblin.source.extractor.ComparableWatermark;
+import gobblin.source.extractor.extract.LongWatermark;
+import gobblin.metrics.Tag;
+import gobblin.source.extractor.extract.kafka.SimpleKafkaSchemaRegistry;
+import gobblin.writer.WatermarkStorage;
+
+
+/**
+ * An implementation of {@link StreamingExtractors} from which reads from Kafka and returns records as an array of bytes.
+ *
+ * @author Shrikanth Shankar
+ *
+ * @deprecated use {@link KafkaDeserializerExtractor} and {@link KafkaDeserializerExtractor.Deserializers#BYTE_ARRAY} instead
+ */
+public class KafkaSimpleStreamingExtractor extends EventBasedExtractor<String, RecordEnvelope<byte[]>> implements StreamingExtractor<String, byte[]>, WatermarkStorage{
+
+    public static class KafkaWatermark implements CheckpointableWatermark {
+      TopicPartition _topicPartition;
+      LongWatermark _lwm;
+
+      KafkaWatermark(TopicPartition topicPartition, LongWatermark lwm) {
+        _topicPartition = topicPartition;
+        _lwm = lwm;
+      }
+
+      @Override
+	    public String getSource() {
+	    return _topicPartition.toString();
+	}
+
+      @Override
+	    public ComparableWatermark getWatermark() {
+	      return _lwm;
+	    }
+
+      @Override
+      public short calculatePercentCompletion(Watermark lowWatermark, Watermark highWatermark) {
+        return 0;
+      }
+
+      @Override
+      public JsonElement toJson() {
+        return null;
+      }
+
+      @Override
+      public int compareTo(CheckpointableWatermark o) {
+        Preconditions.checkArgument(o instanceof KafkaWatermark);
+        KafkaWatermark ko = (KafkaWatermark)o;
+        Preconditions.checkArgument(this._topicPartition.equals(ko._topicPartition));
+        return this._lwm.compareTo(ko._lwm);
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+        if (obj == null)
+          return false;
+        Preconditions.checkArgument(obj instanceof KafkaWatermark);
+        return this.compareTo((CheckpointableWatermark)obj) == 0;
+      }
+
+      @Override
+      public int hashCode() {
+        final int prime = 31;
+        return _topicPartition.hashCode() * prime + _lwm.hashCode();
+      }
+
+      public TopicPartition getTopicPartition() {return _topicPartition;};
+
+      public LongWatermark getLwm() {return  _lwm;};
+    }
+
+  private final KafkaSchemaRegistry<String, String> _kafkaSchemaRegistry;
+  private Consumer<String, byte[]> _consumer;
+  private TopicPartition _partition;
+  private Iterator<ConsumerRecord<String, byte[]>> _records;
+
+  public KafkaSimpleStreamingExtractor(WorkUnitState state) {
+    super(state);
+    this._kafkaSchemaRegistry = new SimpleKafkaSchemaRegistry(state.getProperties());
+    this._consumer = KafkaSimpleStreamingSource.getKafkaConsumer(state);
+    this.closer.register(_consumer);
+    this._partition = new TopicPartition(state.getProp(KafkaSimpleStreamingSource.TOPIC_NAME),
+                                                  state.getPropAsInt(KafkaSimpleStreamingSource.PARTITION_ID));
+    _consumer.assign(Collections.singletonList(_partition));
+    OffsetAndMetadata offset = _consumer.committed(_partition);
+    if (offset == null)
+      _consumer.seekToBeginning(_partition);
+    else
+      _consumer.seek(_partition, offset.offset());
+  }
+
+  /**
+   * Get the schema (metadata) of the extracted data records.
+   *
+   * @return the Kafka topic being extracted
+   * @throws IOException if there is problem getting the schema
+   */
+  @Override
+  public String getSchema() throws IOException {
+    try {
+      return this._kafkaSchemaRegistry.getLatestSchemaByTopic(this._partition.topic());
+    } catch (SchemaRegistryException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public List<Tag<?>> generateTags(State state) {
+    List<Tag<?>> tags = super.generateTags(state);
+    tags.add(new Tag<>("kafkaTopic", state.getProp(KafkaSimpleStreamingSource.TOPIC_WHITELIST)));
+    return tags;
+  }
+
+  /**
+   * Return the next decodable record from the current partition. If the current partition has no more
+   * decodable record, move on to the next partition. If all partitions have been processed, return null.
+   */
+  @Override
+  public RecordEnvelope<byte[]> readRecordImpl(RecordEnvelope<byte[]> reuse) throws DataRecordException, IOException {
+    while ((_records == null) || (!_records.hasNext())) {
+      _records = _consumer.poll(100).iterator();
+    }
+    // eventually do a commit here.
+    return new RecordEnvelope<byte[]>(_records.next().value(), new KafkaWatermark(_partition, new LongWatermark(_records.next().offset())));
+  }
+
+  @Override
+  public long getExpectedRecordCount() {
+    return Long.MAX_VALUE;
+  }
+
+  @Override
+  public void close() throws IOException {
+    closer.close();
+  }
+
+  protected static byte[] getBytes(ByteBuffer buf) {
+    byte[] bytes = null;
+    if (buf != null) {
+      int size = buf.remaining();
+      bytes = new byte[size];
+      buf.get(bytes, buf.position(), size);
+    }
+    return bytes;
+  }
+
+  @Deprecated
+  @Override
+  public long getHighWatermark() {
+    return 0;
+  }
+
+  @Override
+  public void commitWatermarks(Iterable<CheckpointableWatermark> watermarks) {
+    Map<TopicPartition, OffsetAndMetadata> wmToCommit = new HashMap<TopicPartition, OffsetAndMetadata>();
+    for (CheckpointableWatermark cwm : watermarks) {
+      Preconditions.checkArgument(cwm instanceof KafkaWatermark);
+      KafkaWatermark kwm = ((KafkaWatermark)cwm);
+      wmToCommit.put(kwm.getTopicPartition(), new OffsetAndMetadata(kwm.getLwm().getValue(), null));
+    }
+    _consumer.commitSync(wmToCommit);
+  }
+}
---
+++ gobblin-modules/gobblin-kafka-09/src/main/java/gobblin/source/extractor/extract/kafka/KafkaSimpleStreamingSource.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2014-2016 NerdWallet All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package gobblin.source.extractor.extract.kafka;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.Closer;
+
+import gobblin.configuration.ConfigurationKeys;
+import gobblin.configuration.State;
+import gobblin.configuration.WorkUnitState;
+import gobblin.metrics.Tag;
+import gobblin.source.extractor.DataRecordException;
+import gobblin.source.extractor.RecordEnvelope;
+import gobblin.source.extractor.extract.EventBasedSource;
+import gobblin.source.workunit.Extract;
+import gobblin.source.workunit.WorkUnit;
+import gobblin.source.extractor.Extractor;
+import gobblin.configuration.SourceState;
+
+
+/**
+ * A {@link KafkaSource} implementation for SimpleKafkaExtractor.
+ *
+ * @author Shrikanth Shankar
+ *
+ * @deprecated use {@link KafkaDeserializerSource} and {@link KafkaDeserializerExtractor.Deserializers#BYTE_ARRAY} instead
+ */
+public class KafkaSimpleStreamingSource extends EventBasedSource<String, RecordEnvelope<byte[]>> {
+
+  private static final Logger LOG = LoggerFactory.getLogger(KafkaSimpleStreamingSource.class);
+
+  public static final String TOPIC_WHITELIST = "ss.streamingkafka.topic";
+  public static final String TOPIC_NAME = "topic.name";
+  public static final String PARTITION_ID = "partition.id";
+  private final Closer closer = Closer.create();
+  public static final Extract.TableType DEFAULT_TABLE_TYPE = Extract.TableType.APPEND_ONLY;
+  public static final String DEFAULT_NAMESPACE_NAME = "KAFKA";
+
+  static public Consumer<String, byte[]> getKafkaConsumer(State state) {
+
+    List<String> brokers = state.getPropAsList(ConfigurationKeys.KAFKA_BROKERS);
+    Properties props = new Properties();
+    props.put("bootstrap.servers", String.join(",", brokers));
+    props.put("group.id", state.getProp(ConfigurationKeys.JOB_NAME_KEY));
+    props.put("enable.auto.commit", "false");
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+    Consumer consumer = null;
+    try {
+      consumer = new KafkaConsumer<>(props);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return consumer;
+  }
+
+  @Override public List<WorkUnit> getWorkunits(SourceState state) {
+      Consumer<String, byte[]> consumer = getKafkaConsumer(state);
+      LOG.warn("Consumer is " + consumer);
+      String topic = state.getProp(TOPIC_WHITELIST);
+      List<WorkUnit> workUnits = new ArrayList<WorkUnit>();
+      List<PartitionInfo> topicPartitions;
+      topicPartitions = consumer.partitionsFor(topic);
+      LOG.warn("Partition count is " + topicPartitions.size());
+      for (PartitionInfo topicPartition : topicPartitions){
+        LOG.warn("Partition info is " + topicPartition);
+        Extract extract = this.createExtract(DEFAULT_TABLE_TYPE, DEFAULT_NAMESPACE_NAME, topicPartition.topic());
+        WorkUnit workUnit = WorkUnit.create(extract);
+        workUnit.setProp(TOPIC_NAME, topicPartition.topic());
+        workUnit.setProp(ConfigurationKeys.EXTRACT_TABLE_NAME_KEY, topicPartition.topic());
+        workUnit.setProp(PARTITION_ID, topicPartition.partition());
+        workUnits.add(workUnit);
+      }
+      return workUnits;
+    }
+
+
+  /**
+   * Get an {@link Extractor} based on a given {@link WorkUnitState}.
+   * <p>
+   * The {@link Extractor} returned can use {@link WorkUnitState} to store arbitrary key-value pairs
+   * that will be persisted to the state store and loaded in the next scheduled job run.
+   * </p>
+   *
+   * @param state a {@link WorkUnitState} carrying properties needed by the returned {@link Extractor}
+   * @return an {@link Extractor} used to extract schema and data records from the data source
+   * @throws IOException if it fails to create an {@link Extractor}
+   */
+  @Override
+  public Extractor<String, RecordEnvelope<byte[]>> getExtractor(WorkUnitState state) throws IOException {
+    return new KafkaSimpleStreamingExtractor(state);
+    }
+}
